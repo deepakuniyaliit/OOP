ed51babb62746f12e4e7faff0a82117d549bbbc3
Deepak Uniyal <deepak.uniyal08@gmail.com>
2020-07-10 15:02:54 +0530

    add namespace std to the program

--- sum.cpp
+++ sum.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
+using namespace std;
 
 int main(){
 int a, b, sum=0;
-std::cout<<"Enter Numbers"<<std::endl;
-std::cin>>a;
-std::cin>>b;
+cout<<"Enter Numbers\n";
+cin>>a;
+cin>>b;
 sum = a + b;
-std::cout<<"Sum "<<sum<<std::endl;
-return 0;
+cout<<"Sum "<<sum<<"\n";
 }
