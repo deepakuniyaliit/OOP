657d70a56784b5198434e2fd14534ced1ee869b2
Deepak Uniyal <deepak.uniyal08@gmail.com>
2020-07-10 15:00:00 +0530

    sum of two numbers in C++

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+int a, b, sum=0;
+std::cout<<"Enter Numbers"<<std::endl;
+std::cin>>a;
+std::cin>>b;
+sum = a + b;
+std::cout<<"Sum "<<sum<<std::endl;
+return 0;
+}
