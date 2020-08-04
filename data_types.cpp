b1dc9dbbea6b65f319cf99e1c1a8edbc6aff380c
Deepak Uniyal <deepak.uniyal08@gmail.com>
2020-08-04 09:16:03 +0530

    find sizeof data types in C++

---
+++ data_types.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+  cout<<"Size of int "<<sizeof(int)<<" bytes"<<endl;
+  cout<<"Size of char "<<sizeof(char)<<" bytes"<<endl;
+  cout<<"Size of bool "<<sizeof(bool)<<" bytes"<<endl;
+  cout<<"Size of float "<<sizeof(float)<<" bytes"<<endl;
+  cout<<"Size of double "<<sizeof(double)<<" bytes"<<endl;
+  cout<<"Size of wchar_t "<<sizeof(wchar_t)<<" bytes"<<endl;
+}
