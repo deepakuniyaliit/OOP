cc7f950a5e722c45ce8fb1d76999eb15a09f4f9f
Deepak Uniyal <deepak.uniyal08@gmail.com>
2020-08-04 09:15:36 +0530

    destructors in C++

---
+++ destructor.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//GradeBook class definition
+class GradeBook{
+private:
+  string courseName;
+  int year;
+
+public:
+  //Parameterized Constructor
+  GradeBook(string courseName="CSE", int year=1){
+    this->courseName = courseName;
+    this->year = year;
+  }
+
+  //Destructor
+  ~GradeBook(){
+    cout<<this<<" Object is Destroyed"<<endl;
+  }
+
+  string getCourseName(){
+    return courseName;
+  }
+
+  int getYear(){
+    return year;
+  }
+};
+
+//main function starts here
+int main(){
+  GradeBook gb1;
+  GradeBook gb2("Meachanical", 4);
+  cout<<"Object 1 => "<<gb1.getCourseName()<<" => "<< gb1.getYear()<<endl;
+  cout<<"Object 2 => "<<gb2.getCourseName()<<" => "<< gb2.getYear()<<endl;
+}
