6e700cb0e6b9e3b7a3b91da08f707e6106fd35c3
Deepak Uniyal <deepak.uniyal08@gmail.com>
2020-09-06 11:03:02 +0530

    implement arrays in c++

---
+++ arrays.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  //int n[10] = {1,2,3,4,5,6,7,8,9,10};
+  //int n[10] = {};
+  //int n[10] = {1,2,3};
+  int n[] = {1,2,3};
+  // int n[5] = {1,2,3,4,5};
+  for (int i=0; i<10; i++){
+    cout<<n[i]<<" ";
+  }
+  cout<<endl;
+}
